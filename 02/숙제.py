91c6f7a2cee8e105b6206977a11dbdfed0cbb87d
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2022-12-11 18:19:34 +0900

    Update 숙제.py

--- 02/숙제.py
+++ 02/숙제.py
@@ -3,11 +3,22 @@
 
 
 # 2
-s1 = 변수
-s2 = 할당
+s1 = "변수"
+s2 = "할당"
 print((s1+s2)*3)
 
 
-# 3 
+# 3
+string = "여의도역으로 가는 길은 어디인가요?"
+print(string[5]) #로
+print(string[-5]) #어
+print(string[10:12]) #길은 
+print(string[13:]) #어디인가요?
 
 
+# 4
+s = '오늘 평균 기온은 {}도 입니다.
+print(s.format(17))
+print(s.format(18))
+print(s.format(19))
+print(s.format(20))
