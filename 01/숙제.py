6e337de0a838344d207674c570ca50cbca331b7f
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2022-12-11 18:08:54 +0900

    Update 숙제.py

--- 01/숙제.py
+++ 01/숙제.py
@@ -1,7 +1,9 @@
-# 1 print('\'python\'은 프로그랭밍 언어입니다.')
+# 1
+print('\'python\'은 프로그랭밍 언어입니다.')
 
 
-# 2 price1 = 1000
+# 2
+price1 = 1000
 price2 = 96900
 print((price1/100)*5)
 print((price2/100)*5)
