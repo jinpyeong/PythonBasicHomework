7b6fea694766da0e9a4a14844f204a4d27319663
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2022-12-18 19:09:48 +0900

    Create 숙제.py

---
+++ 03/숙제.py
@@ -0,0 +1,25 @@
+#1
+year = input('당신이 태어난 연도는?')
+age = @@@@ - year
+print(age)
+
+
+#2
+def ready_for_school():
+
+  print("아침밥을 먹습니다.")
+  print("양치를 하고 나서 세수를 합니다.")
+  print("등교할 옷으로 갈아입습니다.")
+  print("학교에 가지고 갈 준비물을 챙깁니다.")
+
+ready_for_school()
+
+
+#3
+def go_school():
+
+  print("학교 가방을 맵니다.")
+  print("학교로 갑니다.")
+  
+ready_for_school()
+go_school()
