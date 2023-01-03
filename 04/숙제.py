fa87b262f3d524be8925aefea5dfbf96537e1466
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-01-03 22:08:35 +0900

    Create 숙제.py

---
+++ 04/숙제.py
@@ -0,0 +1,41 @@
+# 1
+a,b
+
+
+# 2
+T
+F
+F
+T
+T
+F
+T
+
+
+# 3
+T
+T
+F
+T
+T
+T
+
+
+# 4
+코딩
+
+
+# 5
+1,3,5
+
+
+# 6
+user = input("")
+if user:
+    print("짝수")
+else:
+    print("홀수")
+
+
+# 7
+?
