07d4a9cc7978d7735da85f4eca53e2f7a46fc03a
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-01-03 22:38:40 +0900

    Create 숙제.py

---
+++ 05/숙제.py
@@ -0,0 +1,59 @@
+# 1
+x = int(input('나이를 입력하세요.'))
+
+if 10 <= x < 20:
+  print('10대')
+elif 20 <= x < 30:
+  print('20대')
+elif 30 <= x < 40:
+  print('30대')
+else :
+  print('10대 미만 40대 이상입니다.')
+  
+  
+# 2
+B
+
+
+# 3
+while True:
+while False:
+  
+  
+# 4
+count = int(input('반복할 횟수를 입력하세요.'))
+
+i = 0
+while i < count:
+    print('Hello, world!' , i)
+    i = i + 1
+    
+    
+# 5
+i = 0
+result_even = 0
+ 
+while @@@:
+    i = i + 1
+    if @@@:
+        result_even = result_even + i
+
+
+# 6
+print('-' * 40)
+print('cm\tmm\tm\tinch')
+print('-' * 40)
+
+cm = 10
+
+while @@@:
+    mm = @@@  # mm 계산
+    m = @@@  # m 계산
+    inch = @@@  # inch 계산
+    
+    print(f'{cm}\t{mm}\t{m}\t{inch}')
+            
+    cm += 10
+    
+print('-' * 40)
+결과
