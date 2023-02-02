8a9beacbdc501b98dc8b1d2967f71c0d2e85dbd1
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-02-02 17:04:21 +0900

    Create 숙제.py

---
+++ 06_중간/숙제.py
@@ -0,0 +1,46 @@
+# 1
+
+print("\'python\'은 프로그래밍 언어입니다.")
+
+# 2
+
+temperature1 = 32
+
+temperature2 = 32
+
+convert1 = (temperature1 - 32) / 18
+convert2 = (temperature2 * 1.8) + 32
+
+print('화씨 {}도는 섭씨 {}도'.format(temperature1, convert1))
+print('섭씨 {}도는 화씨 {}도'.format(temperature2, convert2))
+
+# 3
+
+year = 2023
+
+# 4
+
+3번째줄 , 4번째줄
+
+# 5
+
+n
+
+자
+0과 문자 '10'은 다르다.
+
+# 6
+
+s = '공격받은 몬스터는 {}의 데미지를 받았습니다.'
+print(s.format(10)
+print(s.format(50)
+print(s.format(297)
+
+# 7
+
+age = int(input('\'당신의 나이는?\''))
+year = 2023 - age
+print('당신의 올해 나이는 {}살 입니다. 당신은 {}년에 태어났습니다.'.format(age, year))
+
+# 8
+
