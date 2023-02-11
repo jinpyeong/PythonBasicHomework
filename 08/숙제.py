7926c19b9053224cf7b0b68424be4b71e2794f55
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-02-11 19:56:35 +0900

    Create 숙제.py

---
+++ 08/숙제.py
@@ -0,0 +1,34 @@
+import random
+count = 0 # 이긴 횟수
+
+while True:
+  print('가위 , 바위 , 보 중 하나를 입력하세요')
+  player = @@@
+  if player == '그만':
+    break
+       
+  com = random.randint(1, 3) # 1 ~ 3 중 하나의 숫자 return
+  if com == 1:
+     com = '가위'
+  elif com == 2:
+     com = '바위'
+  elif com == 3:
+     com = '보'
+
+  print('사용자 ( {} vs {} ) 컴퓨터'.format(player, com))
+
+  if player == '가위' or player == '보' or player == '바위':
+        if (com == '가위' and player == '보') or com == '보' or player == '바위':
+            print('컴퓨터 승')
+        elif (com == '바위' and player == '보') or com == '보' or player == '가위':
+            print('플레이어 승')
+            count += 1  # count = count + 1 코드와 동일
+            if count == 3:
+                print("player 3승으로 종료합니다.")
+                break
+        else:
+            print('비겼습니다.')                  
+  if not player == '가위' and '바위' and '보':
+      print("잘못 입력했습니다.")
+      continue
+  
