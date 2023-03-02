f8a60af1ad75f3f0a25ff0c6f2d4b2657f8417a5
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-03-02 17:00:07 +0900

    Update 숙제.py

--- 10/숙제.py
+++ 10/숙제.py
@@ -40,4 +40,32 @@ for price in price_list:
     sale_price = (price / 10) + price
     print("{}원의 결제 금액은 {}원 입니다.".format(price, sale_price))
     
-# 5 (X)
+# 5 ()
+import random
+
+foods = ['짬뽕', '자장면', '탕수육', '자장밥']
+
+title = '''1. 메뉴 추가
+2. 메뉴 삭제
+3. 메뉴 출력
+4. 메뉴 추천
+5. 종료'''
+
+while True:
+    print(title)
+    command = input('어떤 작업을 하시겠습니까?')
+    if command == '1':
+        food = input('추가할 메뉴 : ')
+        foods.append(food)
+    elif command == '2':
+        food = input('삭제할 메뉴 : ')
+        foods.remove(food)
+    elif command == '3':
+        print('='*100)
+        print(foods)
+        print('='*100)
+    elif command == '4':
+        pick = random.choice(foods)
+        print(pick)
+    elif command == '5':
+        break
