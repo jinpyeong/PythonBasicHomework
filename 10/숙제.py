b4a7c6ea023d7b49a613d8a98e1073909a214ba7
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-03-02 16:55:18 +0900

    Update 숙제.py

--- 10/숙제.py
+++ 10/숙제.py
@@ -9,14 +9,14 @@ print('--- 입력된 값 ---')
 for i in range(5):
     print('{}번 학생의 성적: {}'.format(i+1, scores [i]))
     
-# 2 (X)
+# 2 (O)
 scores = [0] * 5
 
 for i in range(5):
     scores[i] = int(input('{}번 학생의 성적을 입력하세요: '.format(i+1)))
 
 for i in range(5):
-    if @@@:
+    if scores[i] >= 60:
         print('{}번 학생 합격'.format(i+1))
     else:
         print('{}번 학생 불합격'.format(i+1))
