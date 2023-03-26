f09aaf677d5061ae5f4acc584485dd827a8d9ae3
jinpyeong <119575245+jinpyeong@users.noreply.github.com>
2023-03-26 22:01:21 +0900

    Create 숙제.py

---
+++ 14/숙제.py
@@ -0,0 +1,41 @@
+# 1 (X)
+# pokemonModule.py
+class Pokemon:
+    def __init__(self, @@@ ...):
+    ....
+
+class Pikachu(Pokemon):
+    def __init__(self, @@@ ...):
+    ....
+    
+class Eevee(@@@):
+    def __init__(self, @@@ ...):
+    ....
+
+
+# main.py
+pikache1 = new Pikache(@@@)
+pikache2 = new Pikache(@@@)
+...
+
+# 2 (O)
+# 새파일.txt
+1 번째 줄입니다.
+2 번째 줄입니다.
+3 번째 줄입니다.
+4 번째 줄입니다.
+5 번째 줄입니다.
+6 번째 줄입니다.
+7 번째 줄입니다.
+8 번째 줄입니다.
+9 번째 줄입니다.
+10 번째 줄입니다.
+
+# 3 (O)
+# 새파일.txt
+안녕하세요.
+
+# 새파일.tx
+
+# 새파일.tx
+안녕하세요.
